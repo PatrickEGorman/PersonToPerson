8cb655c58f0f991f9329ab701380909aa13d9f16
Patrick Gorman <gormanpe@vcu.edu>
2019-04-28 08:17:47 -0400

    initial commit

---
+++ apps/CompareExternal/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CompareexternalConfig(AppConfig):
+    name = 'CompareExternal'
---
+++ apps/PostingOrganizer/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PostingorganizerConfig(AppConfig):
+    name = 'PostingOrganizer'
