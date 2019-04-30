c97aeb938b5712092aa484134e0d11e66384bcdc
Patrick Gorman <gormanpe@vcu.edu>
2019-04-30 19:57:01 -0400

    added web pack functionality

--- static/js/Api/Facebook/login.js
+++ static/js/Api/Facebook/login.js
@@ -2,9 +2,6 @@ function login() {
     FB.login(function (response) {
         if (response.authResponse) {
             console.log('Welcome!  Fetching your information.... ');
-            FB.api('/me', function (response) {
-                console.log('Good to see you, ' + response.name + '.');
-            });
         } else {
             console.log('User cancelled login or did not fully authorize.');
         }
@@ -15,6 +12,7 @@ function checkLoginState() {
   FB.getLoginStatus(function(response) {
     if(response.status === 'connected'){
         document.cookie = "facebook_access_token="+response.authResponse.facebook_access_token;
+        console.log('test');
     }
     else{
         console.log('do something here');
--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from '../lib/node_modules/react'
+import ReactDOM from '../lib/node_modules/react-dom'
 
 
 function getLocation() {
@@ -15,30 +15,3 @@ function showPosition(position) {
       document.cookie = "long="+position.coords.longitude;
   })
 }
-
-function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    if(response.status === 'connected'){
-        document.cookie = "facebook_access_token="+response.authResponse.facebook_access_token;
-    }
-    else{
-        console.log('do something here');
-    }
-  });
-}
-
-window.fbAsyncInit = function() {
-    FB.init({
-      appId            : '298536934407119',
-      autoLogAppEvents : true,
-      xfbml            : true,
-      version          : 'v3.2'
-    });
-    document.getElementById('login_facebook').style.visibility = "visible";
-    FB.api('/me', function(response) {
-        console.log(JSON.stringify(response));
-    });
-    checkLoginState();
-  };
-
-getLocation();
